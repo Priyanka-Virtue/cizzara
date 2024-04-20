5539e71c5a7ddb046f3aa43267d2517a8b292607
Priyanka Sida <priyanka@virtueinfo.com>
2024-04-20 18:23:23 +0530

    random bgs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/materialdesignicons.css') }}" />
-
+@php
+    $bgs = ['5.png', '7.png', '14.png', '6.png', '3.png', '4.png'];
+    $bg = $bgs[array_rand($bgs)];
+@endphp
     <!-- Styles -->
     <style>
         /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */
@@ -720,7 +723,7 @@
         }
 
         .dark\:bg-dots-lighter {
-            background-image: url("{{ asset('images/bg.jpg') }}");
+            background-image: url("{{ asset('images/'.$bg) }}");
             background-repeat: no-repeat;
             background-size: cover;
             /* url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E") */
