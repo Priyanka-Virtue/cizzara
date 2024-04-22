f064c6fd81cb0214400148180c604db08a4b4f64
Priyanka Sida <priyanka@virtueinfo.com>
2024-04-22 14:32:21 +0530

    ico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>The United Production</title>
-
+<link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}" />
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
