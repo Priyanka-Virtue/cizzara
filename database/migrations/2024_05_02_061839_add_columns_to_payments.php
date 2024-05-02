cb896359426905bad1782077689994bef2cb6502
Priyanka Sida <priyanka@virtueinfo.com>
2024-05-02 11:57:42 +0530

    migration

---
+++ database/migrations/2024_05_02_061839_add_columns_to_payments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->string('team_type', 60)->nullable()->after('status');
+            $table->tinyInteger('members')->default(1)->after('team_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn(['team_type', 'members']);
+        });
+    }
+};
