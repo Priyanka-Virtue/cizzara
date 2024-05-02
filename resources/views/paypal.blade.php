85f24b01e96f2eee2f491af27ce8293d1e3d4b78
Priyanka Sida <priyanka@virtueinfo.com>
2024-05-02 12:08:27 +0530

    right side ssl

--- resources/views/paypal.blade.php
+++ resources/views/paypal.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.auth', ['title' => __('Make Payment!'), 'description' => '[lan name]', 'width' => 'col-md-3', 'step' => 'Payment'])
+@extends('layouts.auth', ['title' => __('Make Payment!'), 'description' => '[lan name]', 'width' => 'col-md-6', 'step' => 'Payment'])
 
 @section('content')
     @if (session('status'))
@@ -11,45 +11,50 @@
             This is a sandbox environment, please do not use your real credit/debit card or paypal account.
         </div>
     @endif
-    {{ __('Make Payment!') }}
 
-    <form action="{{ route('processPayment', ['plan' => request()->plan]) }}" method="POST" id="subscribe-form">
-        <div class="form-group">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="subscription-option">
-                        <label for="plan-type">
-                            Select team type
-                        </label>
-
-                        <select name="plan-type" id="plan-type" class="form-select mb-2"
-                            onchange="$(this).planTypeChanged()">
-                            @foreach ($plan['prices'] as $price_name => $plan_amount)
-                                <option value="{{ $price_name }}">
-                                    {{ $price_name . ' $' . $plan_amount['Price'] . ' ' . $plan_amount['Note'] ?? '' }}
-                                </option>
-                            @endforeach
-                        </select>
-
-                        @if (isset($plan['prices']['Group']))
-                            <div id="group-members" style="display: none;">
-                                <label for="members">
-                                    Select group members
+    <div class="row">
+        <div class="col-md-6" style="border-right: 1px solid rgba(100,100,100,0.1);">
+            <form action="{{ route('processPayment', ['plan' => request()->plan]) }}" method="POST" id="subscribe-form">
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="subscription-option">
+                                <label for="plan-type">
+                                    Select team type
                                 </label>
-                                <input type="number" name="members" id="members" value=""
-                                    onchange="$(this).calculateTotal({{ $plan['prices']['Group']['Price'] }})"
-                                    max="{{ $plan['prices']['Group']['MaxMembers'] }}" min="1" class="form-control">
-                                X $ {{ $plan['prices']['Group']['Price'] }} <span id="total"> = $80</span>
-                            </div>
-                        @endif
 
+                                <select name="plan-type" id="plan-type" class="form-select mb-2"
+                                    onchange="$(this).planTypeChanged()">
+                                    @foreach ($plan['prices'] as $price_name => $plan_amount)
+                                        <option value="{{ $price_name }}">
+                                            {{ $price_name . ' $' . $plan_amount['Price'] . ' ' . $plan_amount['Note'] ?? '' }}
+                                        </option>
+                                    @endforeach
+                                </select>
+
+                                @if (isset($plan['prices']['Group']))
+                                    <div id="group-members" style="display: none;">
+                                        <label for="members">
+                                            Select group members
+                                        </label>
+                                        <input type="number" name="members" id="members" value=""
+                                            onchange="$(this).calculateTotal({{ $plan['prices']['Group']['Price'] }})"
+                                            max="{{ $plan['prices']['Group']['MaxMembers'] }}" min="1"
+                                            class="form-control">
+                                        X $ {{ $plan['prices']['Group']['Price'] }} <span id="total"> = $80</span>
+                                    </div>
+                                @endif
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </form>
-    <div id="paypal-button-container"></div>
-    {{-- <form action="{{ route('processPayment', ['plan' => request()->plan]) }}" method="POST" id="subscribe-form">
+            </form>
+            <div id="paypal-button-container"></div>
+
+
+
+            {{-- <form action="{{ route('processPayment', ['plan' => request()->plan]) }}" method="POST" id="subscribe-form">
         <div class="form-group">
             <div class="row">
                 <div class="col-md-12">
@@ -98,26 +103,30 @@
             <button type="button" id="card-button" data-secret="{{ $intent->client_secret }}"
                 class="btn btn-primary d-grid w-100">Make Payment</button>
         </div>
-    </form> --}}
-
-
-
-    <div class="d-flex justify-content-center align-items-center" style="flex-direction: column; margin-top: 2rem">
-
-        {{-- <div class="text-center text-sm sm:text-center"> --}}
-        <img src="{{ asset('images/cards.png') }}" alt="all payment cards">
-        <div class="d-flex justify-content-center align-items-center">
-            <img src="{{ asset('images/secure.png') }}" style="max-width: 110px;" alt="100% secure">
-            <img src="{{ asset('images/ssl-secure.png') }}" style="max-height: 120px;" alt="100% secure">
+        </form> --}}
 
         </div>
-        <hr style="border: 1px solid rgba(100,100,100,0.1);margin-top: 15px; width: 100%;" />
+        <div class="col-md-6">
         <p>Pay with confidence with our <b>encrypted</b> payment system.</p>
-        {{-- </div> --}}
+                {{-- <hr style="border: 1px solid rgba(100,100,100,0.1);margin-top: 15px; width: 100%;" /> --}}
+            <div class="d-flex justify-content-center align-items-center" style="flex-direction: column; margin-top: 2rem">
 
-        {{-- <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                {{-- <div class="text-center text-sm sm:text-center"> --}}
+                <img src="{{ asset('images/cards.png') }}" alt="all payment cards">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('images/secure.png') }}" style="max-width: 110px;" alt="100% secure">
+                    <img src="{{ asset('images/ssl-secure.png') }}" style="max-height: 120px;" alt="100% secure">
+
+                </div>
+
+
+                {{-- </div> --}}
+
+                {{-- <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                     Copyright &copy; <?php echo date('Y'); ?> The United Production.<br/>All rights reserved. Powered by Cizzara Studios.
                 </div> --}}
+            </div>
+        </div>
     </div>
     {{-- <script src="https://js.stripe.com/v3/"></script> --}}
 
