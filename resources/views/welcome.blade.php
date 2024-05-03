210918f1486cb7454fb74f3b85c4e9eb5a7008c3
Priyanka Sida <priyanka@virtueinfo.com>
2024-05-03 11:26:45 +0530

    fix welcome css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -833,6 +833,8 @@
                 grid-template-columns: repeat(2, minmax(0, 1fr))
             }
         }
+        
+
 
         @media (min-width: 1024px) {
             .lg\:gap-8 {
@@ -962,7 +964,7 @@
                                     : 0;
                         @endphp
                         <a @if ($plan_active) href="{{ route('goToPayment', [$plan->name]) }}" @else  style="cursor: not-allowed" href="#" @endif
-                            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="flex items-center rounded-full">
                                     <!-- <span class="mdi mdi-human-female-dance mdi-24px"></span> -->
